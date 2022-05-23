4f68feb496187588f4606edccf5285bfe6dd02eb
Samuel Banya <sbanya@fastmail.com>
2022-05-23 19:19:59 -0400

    Adding initial part of solution 2

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Iterate from the beginning of the string to the middle of the string
+  for (let i = 0; i < word.length /2; i++ ) {
+    // Compare the letter we are iterating over to the corresponding letter at the end of the string
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      // If the letters don't match, return false
+      return false;
+    }
+  }
+  // If we reach the middle, and all the letters match, return true
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
 
 /*
   Add written explanation of your solution here
@@ -12,9 +18,28 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
 
   console.log("");
 
